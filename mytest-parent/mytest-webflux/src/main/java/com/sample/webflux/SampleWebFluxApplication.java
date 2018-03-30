cf89e24a5d2e233beb974f5ffdc8142c455225a7
liucongcong <liucongcong@qq.com>
2018-03-30 18:00:30 +0800

    webflux

---
+++ mytest-parent/mytest-webflux/src/main/java/com/sample/webflux/SampleWebFluxApplication.java
@@ -0,0 +1,24 @@
+package com.sample.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@SpringBootApplication
+public class SampleWebFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleWebFluxApplication.class);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> monoRouterFunction(EchoHandler echoHandler) {
+        return route(POST("/echo"), echoHandler::echo);
+    }
+
+}
