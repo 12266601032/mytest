b86c325f3de5a08c7f5c01974222099a16b7cdf6
liucongcong <liucongcong@qq.com>
2018-07-25 16:56:32 +0800

    asciidoc

--- cors-server/src/main/java/com/example/corsserver/CorsserverApplication.java
+++ cors-server/src/main/java/com/example/corsserver/CorsserverApplication.java
@@ -2,7 +2,9 @@ package com.example.corsserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan(basePackages = "com.example.corsserver.config")
 @SpringBootApplication
 public class CorsserverApplication {
 
--- jsr303-demo/src/test/java/com/example/jsr303demo/controller/UserControllerTest.java
+++ jsr303-demo/src/test/java/com/example/jsr303demo/controller/UserControllerTest.java
@@ -2,15 +2,30 @@ package com.example.jsr303demo.controller;
 
 import com.example.jsr303demo.service.MockUserService;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.halLinks;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
@@ -23,12 +38,34 @@ public class UserControllerTest {
     @Autowired
     private WebApplicationContext context;
 
+    @Rule
+    public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
     @Before
     public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(this.restDocumentation)).build();
     }
 
     @Test
     public void register() throws Exception {
+        preprocessRequest(prettyPrint());
+
+        this.mockMvc
+                .perform(post("/user")
+                        .header("Authorization", "1")
+                        .accept("text/html;charset=utf-8")
+                        .param("username", "abc")
+                        .param("password", "123456"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("必须以xx开头"))
+                .andDo(document("index",
+                        requestHeaders(
+                                headerWithName("Authorization").description(
+                                        "Basic auth credentials")),
+                        responseHeaders(
+                                headerWithName("Content-Type").description(
+                                        "The Content Type of response"))));
 
     }
 
