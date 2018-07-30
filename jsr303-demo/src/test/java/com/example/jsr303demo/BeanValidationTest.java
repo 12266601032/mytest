0e4683a2d30814af25c69a9ed95a0d13550ce1e0
liucongcong <liucongcong@qq.com>
2018-07-30 18:11:27 +0800

    conversion service demo

--- jsr303-demo/src/test/java/com/example/jsr303demo/BeanValidationTest.java
+++ jsr303-demo/src/test/java/com/example/jsr303demo/BeanValidationTest.java
@@ -1,9 +1,20 @@
 package com.example.jsr303demo;
 
 import com.example.jsr303demo.utils.PrintUtils;
+import org.assertj.core.util.Lists;
 import org.hibernate.validator.constraints.Length;
 import org.junit.Test;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.MutablePropertyValues;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.core.convert.support.GenericConversionService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.DataBinder;
 import org.springframework.validation.Errors;
@@ -15,6 +26,13 @@ import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 import javax.validation.constraints.NotNull;
 import javax.validation.executable.ExecutableValidator;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 public class BeanValidationTest {
@@ -53,8 +71,8 @@ public class BeanValidationTest {
                 ValidationUtils.rejectIfEmpty(errors, "username", "ValidationObject");
                 ValidationUtils.rejectIfEmpty(errors, "password", "ValidationObject");
                 ValidationObject object = (ValidationObject) target;
-                if(object.getPassword().length() < 6 || object.getPassword().length() > 10){
-                    errors.rejectValue("password","ValidationObject", "密码长度必须为6~10");
+                if (object.getPassword().length() < 6 || object.getPassword().length() > 10) {
+                    errors.rejectValue("password", "ValidationObject", "密码长度必须为6~10");
                 }
             }
         });
@@ -66,6 +84,62 @@ public class BeanValidationTest {
         dataBinder.validate();
         BindingResult bindingResult = dataBinder.getBindingResult();
         PrintUtils.printBindingResult(bindingResult);
+    }
+
+    @Test
+    public void conversionServiceDemo() {
+        GenericConversionService conversionService = new DefaultConversionService();
+
+        List<Integer> input = Lists.newArrayList(1, 3, 5);
+        List<String> stringList = (List<String>) conversionService.convert(input,
+                TypeDescriptor.forObject(input),
+                TypeDescriptor.collection(List.class, TypeDescriptor.valueOf(String.class)));
+        System.out.println(stringList);
+    }
+
+    @Test
+    public void formattingConversionServiceDemo() throws NoSuchFieldException {
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+        MoneyObject bean = new MoneyObject(BigDecimal.ZERO, new Date());
+        BeanWrapperImpl beanWrapper = new BeanWrapperImpl(bean);
+        for (PropertyDescriptor propertyDescriptor : beanWrapper.getPropertyDescriptors()) {
+            if ("class".equals(propertyDescriptor.getName())) {
+                continue;
+            }
+            Field field = MoneyObject.class.getDeclaredField(propertyDescriptor.getName());
+            Object value = beanWrapper.getPropertyValue(propertyDescriptor.getName());
+            String result = (String) conversionService.convert(value, new TypeDescriptor(field), TypeDescriptor.valueOf(String.class));
+            System.out.println(result);
+        }
+    }
+
+    class MoneyObject {
+
+        @NumberFormat(pattern = "0.00")
+        private BigDecimal money;
+        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private Date date;
+
+        public MoneyObject(BigDecimal money, Date date) {
+            this.money = money;
+            this.date = date;
+        }
+
+        public BigDecimal getMoney() {
+            return money;
+        }
+
+        public void setMoney(BigDecimal money) {
+            this.money = money;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
     }
 
 
